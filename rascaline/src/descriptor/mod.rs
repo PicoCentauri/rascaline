cafcef87cc6ea135a99deeb9642c792c0f1d0691
Guillaume Fraux <guillaume.fraux@epfl.ch>
2021-06-09 15:21:46 +0200

    Add PairSpeciesSamples to build samples for pair descriptors

--- rascaline/src/descriptor/mod.rs
+++ rascaline/src/descriptor/mod.rs
@@ -3,9 +3,14 @@ pub use self::indexes::{Indexes, IndexesBuilder, IndexValue};
 pub use self::indexes::is_valid_index_name;
 
 pub use self::indexes::SamplesBuilder;
-pub use self::indexes::{StructureSamples, AtomSamples};
-pub use self::indexes::{StructureSpeciesSamples, TwoBodiesSpeciesSamples};
-pub use self::indexes::{ThreeBodiesSpeciesSamples};
+
+pub use self::indexes::StructureSamples;
+pub use self::indexes::AtomSamples;
+
+pub use self::indexes::StructureSpeciesSamples;
+pub use self::indexes::TwoBodiesSpeciesSamples;
+pub use self::indexes::ThreeBodiesSpeciesSamples;
+pub use self::indexes::PairSpeciesSamples;
 
 #[allow(clippy::module_inception)]
 mod descriptor;
